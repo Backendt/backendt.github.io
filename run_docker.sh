79d5b9853002477cd14d752c323a62aa4d44020a
Backendt <matisse.beckandt@proton.me>
2024-11-19 18:43:01 +0100

    Added build step to run_docker script

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-docker run -it -dp "4000:4000" --mount type=bind,source="$(pwd)",target=/mnt jekll
+image_name="backendt-blog:latest"
+image_exists=$(docker image inspect $image_name -f 1 2>/dev/null)
+
+if [ -z "$image_exists" ]; then
+  docker build -t $image_name .
+fi
+
+docker run -it -dp "4000:4000" --mount type=bind,source="$(pwd)",target=/mnt $image_name
